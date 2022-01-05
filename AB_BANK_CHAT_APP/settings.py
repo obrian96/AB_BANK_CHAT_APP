cb4dc375c446d86b9a0e91f800036109232bacfe
O'brian Shing'andu <45210100+obrian96@users.noreply.github.com>
2022-01-05 16:47:08 +0200

    fix

--- AB_BANK_CHAT_APP/settings.py
+++ AB_BANK_CHAT_APP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&572q93uq(&pmvpf#5y@-ov03i))=z=v36azejb1negb7kwr*m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
